ac6eb0c7ee4ab3fb818ace26e16210171f41f23c
Alex Wolf <f.alexander.wolf@gmail.com>
2022-05-29 13:15:25 +0200

    🎉 Initial commit

---
+++ biogram/__init__.py
@@ -0,0 +1,18 @@
+"""Make diagrams for biological entities.
+
+Import the package::
+
+   import biogram
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   example_function
+   ExampleClass
+"""
+
+__version__ = "0.1a1"  # pre-release for initial release 0.1.0
+
+from ._core import ExampleClass, example_function  # noqa
