92b79349a5a582e2a199b78a2a13dca835502fbf
Alex Wolf <f.alexander.wolf@gmail.com>
2022-05-29 16:26:30 +0200

    :truck: add sqlalchemy_schemadisplay

--- biogram/__init__.py
+++ biogram/__init__.py
@@ -9,10 +9,10 @@ This is the complete API reference:
 .. autosummary::
    :toctree: .
 
-   example_function
-   ExampleClass
+   create_uml_graph
+   create_schema_graph
 """
 
 __version__ = "0.1a1"  # pre-release for initial release 0.1.0
 
-from ._core import ExampleClass, example_function  # noqa
+from ._sqlalchemy import create_schema_graph, create_uml_graph  # noqa
