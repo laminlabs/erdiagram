8a1d01f3377ddc9a8b9a75ab214c9cc630c26858
Alex Wolf <f.alexander.wolf@gmail.com>
2022-05-30 17:20:20 +0200

    :bug: keep original names for now

--- biogram/__init__.py
+++ biogram/__init__.py
@@ -9,11 +9,11 @@ This is the complete API reference:
 .. autosummary::
    :toctree: .
 
-   create
+   create_schema_graph
    view
 """
 
 __version__ = "0.1a1"  # pre-release for initial release 0.1.0
 
-from ._sqlalchemy import create_schema_graph as create  # noqa
+from ._sqlalchemy import create_schema_graph, create_uml_graph  # noqa
 from ._utils import view
