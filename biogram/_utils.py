b86b3d8b455d7be810fe6644a6b844d3c0a3de87
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-25 16:37:26 +0200

    :children_crossing: Better error message for lacking graphviz

--- biogram/_utils.py
+++ biogram/_utils.py
@@ -2,5 +2,11 @@ from IPython.display import SVG, display
 
 
 def view(pdot):
-    plt = SVG(pdot.create_svg())
-    display(plt)
+    try:
+        plt = SVG(pdot.create_svg())
+        display(plt)
+    except FileNotFoundError:
+        raise RuntimeError(
+            "Visualizing a diagram requires graphviz."
+            "Install, e.g., via sudo apt-get -y install graphviz."
+        )
