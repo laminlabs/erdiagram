c2efba6e4e455db4341baa239f64462b719a7b5f
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-29 17:37:21 +0200

    :bookmark: Release version 0.1.0

--- erdiagram/__init__.py
+++ erdiagram/__init__.py
@@ -1,4 +1,6 @@
-"""Make diagrams for biological entities.
+"""Make diagrams for SQLAlchemy.
+
+Heavily based on the MIT-licensed https://github.com/fschulze/sqlalchemy_schemadisplay!
 
 Import the package::
 
@@ -13,7 +15,7 @@ This is the complete API reference:
    view
 """
 
-__version__ = "0.0.2"
+__version__ = "0.1.0"
 
 from ._sqlalchemy import create_schema_graph, create_uml_graph  # noqa
 from ._utils import view
